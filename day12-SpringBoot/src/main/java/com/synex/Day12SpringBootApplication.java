dbae499a5813a2c1aa27d321305505ebe87c00a9
Alex Kim <ajk8710@nyu.edu>
2023-12-04 14:48:53 -0500

    Loading static (html) & dynamic contents (java GetMapping)

---
+++ day12-SpringBoot/src/main/java/com/synex/Day12SpringBootApplication.java
@@ -0,0 +1,13 @@
+package com.synex;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Day12SpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Day12SpringBootApplication.class, args);
+	}
+
+}
---
+++ day12-SpringBoot/src/main/java/com/synex/controller/WelcomeController.java
@@ -0,0 +1,18 @@
+package com.synex.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class WelcomeController {
+	
+	@GetMapping("/welcome")
+	public String welcome() {
+		return "Welcome to a spring boot";
+	}
+	
+	@GetMapping("/hello")
+	public String hello() {
+		return "Hello dear";
+	}
+}
