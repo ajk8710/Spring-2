ad14520a4eab3ccf08aaeadb7d6732e450b9303b
Alex Kim <ajk8710@nyu.edu>
2023-12-14 15:06:34 -0500

    Testing CRUD APIs with Postman & H2. H2 is spring inbuilt database that maintains data in volatile memory for testing purpose.

---
+++ day17-H2/src/main/java/com/synex/service/Student17Service.java
@@ -0,0 +1,28 @@
+package com.synex.service;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.synex.domain.Student17;
+import com.synex.repository.Student17Repository;
+
+@Service
+public class Student17Service {
+    
+    @Autowired Student17Repository student17Repository;
+    
+    public List<Student17> findAll() {
+        return student17Repository.findAll();
+    }
+    
+    public Student17 save(Student17 student) {
+        return student17Repository.save(student);
+    }
+    
+    public void deleteById(int studentId) {
+        student17Repository.deleteById(studentId);
+    }
+    
+}
