7eb0e7d18c6a3da6db1a2e015dd0809c76551b8e
Alex Kim <ajk8710@nyu.edu>
2024-02-03 19:55:50 -0500

    Booking status updates from UPCOMING to COMPLETED if checkInDate has past, when user refreshes My Bookings.

--- TravelGig-BookingMicroService/src/main/java/com/synex/service/BookingServiceImpl.java
+++ TravelGig-BookingMicroService/src/main/java/com/synex/service/BookingServiceImpl.java
@@ -1,13 +1,16 @@
 package com.synex.service;
 
+import java.time.Instant;
+import java.time.LocalDate;
+import java.time.ZonedDateTime;
+import java.time.temporal.ChronoUnit;
 import java.util.List;
-import java.util.Set;
+import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.synex.domain.Booking;
-import com.synex.domain.Guest;
 import com.synex.repository.BookingRepository;
 
 @Service
@@ -17,8 +20,11 @@ public class BookingServiceImpl implements BookingService {
     
     @Override
     public Booking save(Booking booking) {
-        Set<Guest> guestList = booking.getGuests();
-        System.out.println(guestList);
+        // Set<Guest> guestList = booking.getGuests();
+        // System.out.println(guestList);
+        
+        Instant bookedTime = ZonedDateTime.now().toInstant().truncatedTo(ChronoUnit.DAYS);
+        booking.setBookedOnDate(bookedTime.toString());
         
         return bookingRepository.save(booking);
     }
@@ -32,9 +38,28 @@ public class BookingServiceImpl implements BookingService {
     public boolean existById(int id) {
         return bookingRepository.existsById(id);
     }
-
+    
     @Override
     public List<Booking> findAllByUserName(String userName) {
+        List<Booking> myBookings = bookingRepository.findAllByUserName(userName);
+        
+        List<Integer> upcomingBookings = myBookings.stream()
+                .filter(b -> b.getStatus().equals("UPCOMING"))  // Filter upcoming bookings to check if checkInDate is past current time.
+                .map(b -> b.getBookingId())
+                .collect(Collectors.toList());
+
+        for (int id : upcomingBookings) {  // Change status of upcoming bookings if checkInDate is past current time.
+            Booking booking = bookingRepository.findById(id).orElse(null);
+            LocalDate checkInDate = LocalDate.of(Integer.parseInt(booking.getCheckInDate().substring(0, 4)),
+                                                 Integer.parseInt(booking.getCheckInDate().substring(5, 7)),
+                                                 Integer.parseInt(booking.getCheckInDate().substring(8)));
+            System.out.println("Upcoming Booking ID: " + id + ", CheckinDate: " + checkInDate);
+            if (checkInDate.isBefore(LocalDate.now())) {
+                booking.setStatus("COMPLETED");
+                bookingRepository.save(booking);
+            }
+        }
+        
         return bookingRepository.findAllByUserName(userName);
     }
     
