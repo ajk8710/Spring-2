decb35985cd2cb7d27b27a2692d5c9df39992299
Alex Kim <ajk8710@nyu.edu>
2024-02-12 09:10:14 -0500

    Insurance Project set up

---
+++ InsuranceExpress/src/main/java/com/synex/InsuranceExpressApplication.java
@@ -0,0 +1,13 @@
+package com.synex;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InsuranceExpressApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InsuranceExpressApplication.class, args);
+	}
+
+}
---
+++ InsuranceExpress/src/main/java/com/synex/controller/HomeController.java
@@ -0,0 +1,29 @@
+package com.synex.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller  // RestController returns json data. Controller returns view resolver (returns file name).
+public class HomeController {
+    
+    @GetMapping("index")
+    public String index() {
+        return "index";
+    }
+    
+    @GetMapping("blog")
+    public String blog() {
+        return "blog";
+    }
+    
+    @GetMapping("blog-details")
+    public String blogDetails() {
+        return "blog-details";
+    }
+    
+    @GetMapping("contact-us")
+    public String contactUs() {
+        return "contact-us";
+    }
+    
+}
